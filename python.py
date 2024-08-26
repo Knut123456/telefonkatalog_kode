bcf2bb9ce1265c601da8b58dda9069a3f1f43795
Knut123456 <knut.rote@gmail.com>
2024-08-26 09:50:00 +0200

    Initial commit

---
+++ python.py
@@ -0,0 +1,30 @@
+telefonkatalog = [] #listeformat ["fornavn", "etternavn" , "telephonnummer"]
+
+def printMeny ():
+    print ("---------------------- Telefonkatalog ----------------------")
+    print (" 1. legg til en ny person")
+    print (" 2. Søk opp person eller telefonnummer")
+    print (" 3. vis alle personer")
+menyvalg = input ("skriv inn tall for å velge fra menyen")
+utfoermenyvalg(menyvalg)
+
+
+def utfoermenyvalg (valgtall):
+    if valgtall == "1":
+        registrerPerson()
+    elif valgtall == "2":
+        sokPerson()
+        printMeny()
+    elif valgtall == "3":
+        visallePersonar()
+    elif valgtall == "4":
+        bekreftelse = input ("Er du sikker på at du vil avslutte? J/N ")
+        if (bekreftelse =="j" or bekreftelse =="j"): # skjekker bare fo ja
+            exit()
+    else:
+        nyttforsoek = input ("Ulydig valg. velg en tall mellom 1-4 ")
+        utfoermenyvalg (nyttforsoek)
+
+def registrerPerson():
+    fornavn
+
